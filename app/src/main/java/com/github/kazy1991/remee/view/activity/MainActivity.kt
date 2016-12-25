2f7f160dcbf5ad4c56a5e1c787bea0146d536e84
kazuki-yoshida <kzk.yshd@gmail.com>
2016-12-25 12:19:18 +0900

    Move MainActivity Module

--- app/src/main/java/com/github/kazy1991/remee/view/activity/MainActivity.kt
+++ app/src/main/java/com/github/kazy1991/remee/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.kazy1991.remee
+package com.github.kazy1991.remee.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kazy1991.remee.R
 
 class MainActivity : AppCompatActivity() {
 
