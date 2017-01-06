09c6ddb18e1edae2a27883174cb299f5676495c6
kazuki-yoshida <kzk.yshd@gmail.com>
2017-01-06 21:34:59 +0900

    Connect TwitterPack test

--- app/src/main/java/com/github/kazy1991/remee/view/activity/MainActivity.kt
+++ app/src/main/java/com/github/kazy1991/remee/view/activity/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.github.kazy1991.remee.view.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.kazy1991.remee.BuildConfig
 import com.github.kazy1991.remee.R
+import com.github.kazy1991.twitterpack.TwitterAuthConfig
+import com.github.kazy1991.twitterpack.TwitterPackImpl
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    val twitter by lazy {
+        TwitterPackImpl(this, authConfig)
+    }
+
+    val authConfig by lazy {
+        TwitterAuthConfig(BuildConfig.CONSUMER_KEY, BuildConfig.CONSUMER_SECRET)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById(R.id.text_view).setOnClickListener() {
+            twitter
+                    .fetchFavorite("101kaz")
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        it.size
+                    }, {
+                        it.cause
+                    })
+        }
     }
 }
